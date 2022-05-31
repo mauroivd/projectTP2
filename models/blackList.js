473150408b2194d09d640130a15d36d164fa5cc3
FdValls <fernandodanielvalls@gmail.com>
2022-05-31 14:59:58 -0300

    nueva estructura de diseño

---
+++ models/blackList.js
@@ -0,0 +1,7 @@
+module.exports = function (id, nombre, direccion, cantidad) {
+    // this.id = id
+    // this.nombre = nombre
+    // this.direccion = direccion
+    // this.cantidad = cantidad
+
+}
